fb9763787268f61b668081b2b6e291025a2e0f42
chesmore <chesmore@uchicago.edu>
2022-07-15 17:22:11 -0500

    fixing setup script

--- .ipynb_checkpoints/setup-checkpoint.py
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -51,33 +51,45 @@
 # )
 
 
+
 import os
-from setuptools import setup, Command, find_packages
+import pathlib
+from distutils.core import setup
 
-def readme():
-    with open('README.md') as f:
-        return f.read()
+import pkg_resources
+import setuptools
 
-class CleanCommand(Command):
-    """Custom clean command to tidy up the project root."""
-    user_options = []
-    def initialize_options(self):
-        pass
-    def finalize_options(self):
-        pass
-    def run(self):
-        os.system('rm -vrf build dist *.pyc *.tgz *.egg-info')
+VERSION = "0.1"
 
 
-setup(name = 'sosat_optics',
-      version = '0.1',
-      description = '''Code for Modeling Polarization in Optics''',
-      long_description = readme(),
-      author = 'Katie Harrington',
-      author_email = 'katie.megan.harrington@gmail.com',
-      license = 'MIT',
-      packages = ['sosat_optics'],
-      package_dir = {'sosat_optics':'sosat-optics'},
-      cmdclass={'clean':CleanCommand,},
-      #scripts = []
-      )
+def build_packages(base_dir, name_base):
+    """
+    recusively find all the folders and treat them as packages
+    """
+    arr = [name_base]
+    for fname in os.listdir(base_dir):
+        if os.path.isdir(base_dir + fname):
+            """
+            ignore the hidden files
+            """
+            if fname[0] == ".":
+                continue
+            name = "{}.{}".format(name_base, fname)
+            recursion = build_packages(base_dir + fname + "/", name)
+            if len(recursion) != 0:
+                [arr.append(rec) for rec in recursion]
+    return arr
+
+
+packages = build_packages("sosat_optics/", "sosat_optics")
+
+setup(
+    name="sosat_optics",
+    version=VERSION,
+    description="Software for data acquisition of the holography setup.",
+    author="Grace E. Chesmore, UChicago Lab",
+    author_email="chesmore@uchicago.edu",
+    package_dir={"sosat_optics": "sosat_optics"},
+    packages=packages
+)
+
