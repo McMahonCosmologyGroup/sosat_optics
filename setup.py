25deeb233f989bc7ea790ce4d80930c6a67d315a
agthomas-uc <agthomas@uchicago.edu>
2023-09-24 00:24:33 -0500

    replaced build_package with setuptools.find_packages

--- setup.py
+++ setup.py
@@ -1,27 +1,6 @@
 import os
 from setuptools import setup, find_packages
 
-def build_packages(base_dir, name_base):
-    """
-    recusively find all the folders and treat them as packages
-    """
-    arr = [name_base]
-    for fname in os.listdir(base_dir):
-        if os.path.isdir(base_dir + fname):
-            """
-            ignore the hidden files
-            """
-            if fname[0] == ".":
-                continue
-            name = "{}.{}".format(name_base, fname)
-            recursion = build_packages(base_dir + fname + "/", name)
-            if len(recursion) != 0:
-                [arr.append(rec) for rec in recursion]
-    return arr
-
-
-packages = build_packages("sosat_optics/", "sosat_optics")
-
 setup(
     name="sosat_optics",
     version="0.1.1",
@@ -30,7 +9,6 @@ setup(
     long_description_content_type="text/markdown",
     author="Grace E. Chesmore, Alex Thomas, UChicago Lab",
     author_email="chesmore@uchicago.edu, agthomas@uchicago.edu",
-    packages=packages,
-    package_dir={"sosat_optics": "sosat_optics"},
+    packages=find_packages(exclude=["tests*"]),
     install_requires=["numpy", "matplotlib", "scipy==1.10.1"]
 )
